9692a1591331bf2a4b777331a231c627dd05472f
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2024-12-03 15:42:56 -0300

    Adiciona calculadora

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número: ");
+        double numero1 = input.nextInt();
+        System.out.println("Digite o segundo número: ");
+        double numero2 = input.nextDouble();
+        System.out.println("Digite qual operação realizar (+, -, /, *, %)");
+        char operador = input.next().charAt(0);
+
+        if (operador == '+') {
+            System.out.println(numero1 + " + " + numero2 + " = " + (numero1 + numero2));
+        } else if (operador == '-') {
+            System.out.println(numero1 + " - " + numero2 + " = " + (numero1 - numero2));
+        } else if (operador == '/') {
+            System.out.println(numero1 + " / " + numero2 + " = " + (numero1 / numero2));
+        } else if (operador == '*') {
+            System.out.println(numero1 + " * " + numero2 + " = " + (numero1 * numero2));
+        } else if (operador == '%') {
+            System.out.println(numero1 + " % " + numero2 + " = " + (numero1 % numero2));
+        } else {
+            System.out.println("Operador inválido! Escreva uma das opções apresentadas.");
+        }
+    }
+}
